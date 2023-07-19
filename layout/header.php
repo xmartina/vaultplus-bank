7de918836dd047ba5a6ac0cc9bec9a0bb249f929
unknown <martinsprince126@gmail.com>
2023-07-19 17:16:31 +0100

    Modified: sit icon on login page

--- layout/header.php
+++ layout/header.php
@@ -25,7 +25,7 @@ $sendMail = new emailMessage();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title><?=$pageTitle ?> - Login </title>
-    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="/assets/images/logo/favicon.png"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
